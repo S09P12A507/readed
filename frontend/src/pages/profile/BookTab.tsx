0e46d4f1f423debdd37e9f0da7c40bd3f0722e83
박성준 <spmjoon516@naver.com>
2023-07-27 12:58:02 +0900

    [FE/feat] framer-motion 라이브러리 설치 ()

---
+++ frontend/src/pages/profile/BookTab.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+// 기본적인 구조입니다.
+const Container = styled.div``;
+
+function BookTab() {
+  return (
+    <Container>
+      <div />
+    </Container>
+  );
+}
+
+export default BookTab;
---
+++ frontend/src/pages/profile/ReportTab.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+// 기본적인 구조입니다.
+const Container = styled.div``;
+
+function ReportTab() {
+  return (
+    <Container>
+      <div />
+    </Container>
+  );
+}
+
+export default ReportTab;
