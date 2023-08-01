0ec3e36d01e1cb71b915404251074734cc848145
박성준 <spmjoon516@naver.com>
2023-08-01 17:24:51 +0900

    [FE/feat] feat/33 해결한 부분에서 추가적인 오류 수정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,11 +5,11 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   // <React.StrictMode>
-  <App />
+  <App />,
   // </React.StrictMode>
 );
 
