4ae8c109674e15b617bc902b45f41cd6e68ec3fb
이남곤 <nglee.dev@gmail.com>
2023-08-04 17:01:51 +0900

    [BE/fix] Book 엔티티 수정

--- backend/src/main/java/ssafy/readed/domain/book/entity/Book.java
+++ backend/src/main/java/ssafy/readed/domain/book/entity/Book.java
@@ -1,24 +1,16 @@
 package ssafy.readed.domain.book.entity;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import ssafy.readed.domain.report.entity.Report;
 import ssafy.readed.global.entity.BaseEntity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -45,24 +37,21 @@ public class Book extends BaseEntity {
 
     private LocalDateTime publishedDate;
 
-    private Long pages;
+    private Long pageCount;
 
     private Boolean isAdult;
 
-    @Column(name = "book_cover")
-    private String cover;
-
-    private Long commentCount;
+    private String coverImage;
 
     private String packingStyle;
 
-    private Long weight;
+    private Long sizeWeight;
 
     private Long sizeDepth;
 
     private Long sizeHeight;
 
-    private Long sizeWeight;
+    private Long sizeWidth;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_code_id")
@@ -80,30 +69,37 @@ public class Book extends BaseEntity {
     private List<BookAuthor> bookAuthorList = new ArrayList<>();
 
     @OneToMany(mappedBy = "book", fetch = FetchType.LAZY)
-    private List<Report> reports = new ArrayList<>();
+    private List<Report> reportList = new ArrayList<>();
 
+    private Long commentCount = 0L;
+    private Long avgRating = 0L;
+    private Long star_0p5_count = 0L;
+    private Long star_1_count = 0L;
+    private Long star_1p5_count = 0L;
+    private Long star_2_count = 0L;
+    private Long star_2p5_count = 0L;
+    private Long star_3_count = 0L;
+    private Long star_3p5_count = 0L;
+    private Long star_4_count = 0L;
+    private Long star_4p5_count = 0L;
+    private Long star_5_count = 0L;
 
     @Builder
-    public Book(String title, String originalTitle, String subtitle, String description, String toc,
-            LocalDateTime publishedDate, Long pages, Boolean isAdult, String cover,
-            Long commentCount,
-            String packingStyle, Long weight, Long sizeDepth, Long sizeHeight, Long sizeWeight,
-            BookCode bookCode, Category category, Publisher publisher) {
+    public Book(String title, String originalTitle, String subtitle, String description, String toc, LocalDateTime publishedDate, Long pageCount, Boolean isAdult, String coverImage, String packingStyle, Long sizeWeight, Long sizeDepth, Long sizeHeight, Long sizeWidth, BookCode bookCode, Category category, Publisher publisher) {
         this.title = title;
         this.originalTitle = originalTitle;
         this.subtitle = subtitle;
         this.description = description;
         this.toc = toc;
         this.publishedDate = publishedDate;
-        this.pages = pages;
+        this.pageCount = pageCount;
         this.isAdult = isAdult;
-        this.cover = cover;
-        this.commentCount = commentCount;
+        this.coverImage = coverImage;
         this.packingStyle = packingStyle;
-        this.weight = weight;
+        this.sizeWeight = sizeWeight;
         this.sizeDepth = sizeDepth;
         this.sizeHeight = sizeHeight;
-        this.sizeWeight = sizeWeight;
+        this.sizeWidth = sizeWidth;
         this.bookCode = bookCode;
         this.category = category;
         this.publisher = publisher;
